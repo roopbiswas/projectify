549fdef54a769a0c98aa5980e7ba8336c4e03a36
Rajan Jha <jharajan20@gmail.com>
2019-03-23 18:48:44 +0530

    changed the app name

--- projectify/projectify/settings.py
+++ projectify/projectify/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'allot.apps.AllotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #our apps
+    'allot',
 ]
 
 MIDDLEWARE = [
